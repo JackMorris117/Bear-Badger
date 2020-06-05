8dee5a4eb7734bc80fc0150f39339c803e49105e
Jack Morris <morriseo94@gmail.com>
2020-06-05 17:32:40 +0100

    start on front end

---
+++ controller.rb
@@ -0,0 +1,28 @@
+require('sinatra')
+require('sinatra/contrib/all') 
+require_relative('./models/book')
+require_relative('./models/publisher')
+require_relative('./models/genre')
+also_reload('./models/*')
+
+get '/BearAndBadger' do
+    erb( :home )
+end
+get '/BearAndBadger/all-books' do
+    @books = Book.all
+    erb(:all_books)
+end
+get '/BearAndBadger/about-us' do
+    erb( :about_us )
+end
+get 'BearAndBadger/:id'do 
+    @book = Book.find(params[:id])
+    erb( :show )
+end
+get '/BearAndBadger/:id/edit' do
+    @publishers = Publisher.all
+    @genres = Genre.all
+
+    @book = Book.find(params['id'])
+    erb(:edit)
+  end
