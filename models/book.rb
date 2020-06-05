4bb653b0968b4c070522e56e690fb0c3e15f70bd
Jack Morris <morriseo94@gmail.com>
2020-06-05 14:24:00 +0100

    book-semidone

--- models/book.rb
+++ models/book.rb
@@ -13,7 +13,7 @@ class Book
         @price = options['price']
     end
     def save()
-        sql = "INSET INTO books
+        sql = "INSERT INTO books
         (
             name,
             author,
@@ -31,5 +31,51 @@ class Book
         id = result.first['id']
         @id = id
     end
+
+    def update()
+        sql = "UPDATE books SET
+        (
+            name,
+            author,
+            publisher_id,
+            genre_id,
+            price
+        ) =
+        (
+            $1, $2, $3, $4, $5
+        )
+        WHERE id = $6"
+        values = [name, author, publisher_id, genre_id, price]
+        SqlRunner.run(sql, values)
+    end
+
+    def delete()
+        sql = "DELETE FROM books
+        WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
+    def self.all()
+        sql = "SELECT * FROM books"
+        book_data = SqlRunner.run(sql)
+        books = map_items(book_data)
+        return books
+    end
+
+    def self.map_items(book_data)
+        return book_data.map { |book| book.new(book) }
+    end
+    def self.find(id)
+        sql = "SELECT * FROM books
+        WHERE id = $1"
+        values = [id]
+        result = SqlRunner.run(sql, values).first
+        book = Book.new(result)
+        return book
+    end
+
+    
+
 
 end
